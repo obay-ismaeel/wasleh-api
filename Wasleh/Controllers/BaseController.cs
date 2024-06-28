f9cecb8748ff791e1c56c194a5b302389907824c
Obay Ismaeel <obayhany@gmail.com>
2024-06-28 14:59:38 +0300

    Create base controller

---
+++ Wasleh/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Wasleh.Domain.Abstractions;
+using Wasleh.Presistence.Data;
+
+namespace Wasleh.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseController : ControllerBase
+{
+    protected readonly IUnitOfWork _unitOfWork;
+    protected readonly IMapper _mapper;
+    public BaseController(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+}
