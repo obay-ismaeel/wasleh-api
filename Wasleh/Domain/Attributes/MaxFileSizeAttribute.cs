b6c6b65c80658739716927009e3114c1814afbe5
Obay Ismaeel <obayhany@gmail.com>
2024-06-29 08:20:59 +0300

    Add file validation attributes

---
+++ Wasleh/Domain/Attributes/MaxFileSizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wasleh.Domain.Attributes;
+public class MaxFileSizeAttribute : ValidationAttribute
+{
+    private readonly int _maxFileSize;
+    public MaxFileSizeAttribute(int maxFileSize)
+    {
+        _maxFileSize = maxFileSize;
+    }
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var file = value as IFormFile;
+
+        if (file is not null)
+        {
+            if (file.Length > _maxFileSize)
+            {
+                return new ValidationResult($"File size can't exceed {_maxFileSize} bytes");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
